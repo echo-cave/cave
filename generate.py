babfc2c107977fb3d102488f3de55bbba83ed8e1
RDPStudio <67402215+rdp-studio@users.noreply.github.com>
2021-08-24 10:22:00 +0800

    Add files via upload

---
+++ generate.py
@@ -0,0 +1,20 @@
+#encoding: utf-8
+import os
+fdirPath = "categories"
+categories = ["general"]
+res = ""
+for cat in categories:
+    dirPath = fdirPath + "\\" + cat
+    files = os.listdir(dirPath)
+    for file in files:
+        if file.endswith(".txt"):
+            with open(dirPath + "\\" + file, "r", encoding='utf-8') as f:
+                content = f.read()
+                f.close()
+            append = "%s\n" % (content)
+            res += append
+
+with open("cave.txt", "w", encoding='utf-8') as outFile:
+    outFile.write(res)
+    outFile.close()
+print(len(res))
