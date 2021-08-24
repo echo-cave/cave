9f4751c4cb73fb628437d9d947d97520db6779a5
RDPStudio <67402215+rdp-studio@users.noreply.github.com>
2021-08-24 13:24:24 +0800

    Update generate.py

--- generate.py
+++ generate.py
@@ -10,11 +10,11 @@ for cat in categories:
         if file.endswith(".txt"):
             with open(dirPath + "/" + file, "r", encoding='utf-8') as f:
                 content = f.read()
-                content.strip()
+                content = content.strip()
                 f.close()
             append = "%s\n" % (content)
             res += append
-            res.strip()
+            content = res.strip()
 
 with open("cave.txt", "w", encoding='utf-8') as outFile:
     outFile.write(res)
