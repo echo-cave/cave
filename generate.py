08bc0123d3fcf1af6a2217ca26361e8088c75c48
RDPStudio <67402215+rdp-studio@users.noreply.github.com>
2021-08-24 10:24:54 +0800

    Update generate.py

--- generate.py
+++ generate.py
@@ -4,11 +4,11 @@ fdirPath = "categories"
 categories = ["general"]
 res = ""
 for cat in categories:
-    dirPath = fdirPath + "\\" + cat
+    dirPath = fdirPath + "/" + cat
     files = os.listdir(dirPath)
     for file in files:
         if file.endswith(".txt"):
-            with open(dirPath + "\\" + file, "r", encoding='utf-8') as f:
+            with open(dirPath + "/" + file, "r", encoding='utf-8') as f:
                 content = f.read()
                 f.close()
             append = "%s\n" % (content)
